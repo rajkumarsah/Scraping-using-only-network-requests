57397040479f86cd7d20ee951fdd85db04d97235
RAJ <rajkumarsah30@gmail.com>
2022-06-15 17:57:06 +0545

    Pagination added

--- annapurnapost.py
+++ annapurnapost.py
@@ -3,32 +3,40 @@ import json
 
 payload={}
 headers = {}
-pageNo=1
 
-try:
+errorChecking = requests.request("GET", "https://bg.annapurnapost.com/api/news/list?page=1&category_alias=bharkharai&per_page=30", headers=headers, data=payload)    
+
+print(errorChecking.status_code)
+
+def pagination(pageNo):
+        
     response = requests.request("GET", "https://bg.annapurnapost.com/api/news/list?page={}&category_alias=bharkharai&per_page=30".format(pageNo), headers=headers, data=payload)
-except Exception as e:
-    pageNo+=1
-    response = requests.request("GET", "https://bg.annapurnapost.com/api/news/list?page={}&category_alias=bharkharai&per_page=30".format(pageNo), headers=headers, data=payload)
-    
 
+    jsonData = json.loads(response.text)
 
-jsonData = json.loads(response.text)
+    data = {}
+    i=1
+    for article in jsonData['data']:
+        articleId = article['id']
+        articleTitle = article['title']
+        articlePublishOn = article['publishOn']
+        oneData={}
+        oneData['id']=articleId
+        oneData['title'] = articleTitle
+        oneData['publishOn'] = articlePublishOn
+        data['article'+str(i)] = oneData
+        i+=1
 
-data = {}
-i=1
-for article in jsonData['data']:
-    articleId = article['id']
-    articleTitle = article['title']
-    articlePublishOn = article['publishOn']
-    oneData={}
-    oneData['id']=articleId
-    oneData['title'] = articleTitle
-    oneData['publishOn'] = articlePublishOn
-    data['article'+str(i)] = oneData
-    i+=1
+    print(data)
 
-print(data)
+    #Storing scraped data in json file!!
 
-with open('30_article.json', 'w') as fp:
-    json.dump(data, fp, indent=4)
+    with open('30_article.json', 'w') as fp:
+        json.dump(data, fp, indent=4)
+
+#Scrapping from second page if error occured!!
+
+if errorChecking.status_code==200:
+    pagination(1)
+else:
+    pagination(2)
